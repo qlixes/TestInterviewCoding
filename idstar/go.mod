a7ffdf9c5174f41773ee9385605a74d9fb0ec81a
Tuntaz Dev 140 <qlixess@gmail.com>
2025-05-21 17:06:41 +0700

    feat: add idstar

---
+++ idstar/go.mod
@@ -0,0 +1,3 @@
+module github.com/qlixes/idstar
+
+go 1.23.4
---
+++ idstar/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func RotateNum(stack []int) string {
+
+	size := len(stack)
+	result := make([]string, size)
+
+	mode := stack[0]
+	id := 0
+
+	if len(stack) < mode {
+		return strings.Join(result, "")
+	}
+
+	for i, j := range stack {
+
+		if i < mode {
+			id = size - mode + i
+		}
+
+		if i >= mode {
+			id = i - mode
+		}
+
+		result[id] = strconv.Itoa(j)
+	}
+
+	return strings.Join(result, "")
+}
+
+func main() {
+	fmt.Println(RotateNum([]int{3,4,6,7,1,5,6,7,8,9}))
+}

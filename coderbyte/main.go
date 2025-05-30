d755b76635f8dae1e754a779510ce6c8d592524e
Antony A <qlixess@gmail.com>
2025-05-30 13:47:30 +0700

    update coderbyte LongestWord

--- coderbyte/main.go
+++ coderbyte/main.go
@@ -1,26 +1,28 @@
-package coderbyte
+package main
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
 
 func LongestWord(sen string) string {
-
-	re := regexp.MustCompile(`[^a-zA-Z0-9 ]`)
+	re := regexp.MustCompile(`[^a-zA-Z0-9 ]+`)
 	clean := re.ReplaceAllString(sen, "")
 
 	stack := strings.Split(clean, " ")
 
 	result := ""
+
 	for _, j := range stack {
 		if len(result) < len(j) {
 			result = j
 		}
 	}
+
 	return result
 }
 
 func main() {
-
+	fmt.Println(LongestWord("123456789 98765432"))
 }

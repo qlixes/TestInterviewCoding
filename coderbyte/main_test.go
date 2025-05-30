c7dd0bca6873905809e08878d0b542e7a5a1b60a
Antony A <qlixess@gmail.com>
2025-05-30 14:39:17 +0700

    add main unit testing

--- coderbyte/main_test.go
+++ coderbyte/main_test.go
@@ -1,4 +1,4 @@
-package coderbyte
+package main
 
 import "testing"
 
@@ -89,5 +89,95 @@ func TestLongestWordCase10(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestFirstFactorial01(t *testing.T) {
+	got := FirstFactorial(1)
+	want := 1
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial02(t *testing.T) {
+	got := FirstFactorial(2)
+	want := 2
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial03(t *testing.T) {
+	got := FirstFactorial(3)
+	want := 6
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial04(t *testing.T) {
+	got := FirstFactorial(4)
+	want := 24
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial05(t *testing.T) {
+	got := FirstFactorial(5)
+	want := 120
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial06(t *testing.T) {
+	got := FirstFactorial(6)
+	want := 720
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial07(t *testing.T) {
+	got := FirstFactorial(7)
+	want := 5040
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial08(t *testing.T) {
+	got := FirstFactorial(8)
+	want := 40320
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial09(t *testing.T) {
+	got := FirstFactorial(9)
+	want := 362880
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestFirstFactorial10(t *testing.T) {
+	got := FirstFactorial(10)
+	want := 3628800
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
 	}
 }
